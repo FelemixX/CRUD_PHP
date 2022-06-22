afff45819762aff7185534508db9210c1788beb8
FelemixX <83292474+FelemixX@users.noreply.github.com>
2022-06-22 16:26:46 +0300

    Image crop added

--- documents/upload.php
+++ documents/upload.php
@@ -7,7 +7,6 @@ if (!isset($_SESSION["usedId"])) {
 }
 
 require_once('../config/Database.php');
-require_once('../tables/document.php');
 
 $db = new Database();
 $conn = $db->getConnection();
@@ -39,7 +38,7 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
         $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
 
         // проверить, имеет ли файл одно из этих расширений
-        $allowedFileExtensions = array('jpg', 'docx');
+        $allowedFileExtensions = array('docx');
 
         if (in_array($fileExtension, $allowedFileExtensions)) {
             // директория, в которую будет сохранен файл
@@ -145,4 +144,109 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
             </div>
         </div>
     </form>
+    <!-- Crop modal -->
+    <div class="container" align="center">
+        <div class="row">
+            <div class="col-md-4">&nbsp;</div>
+            <div class="col-md-4">
+                <div class="image_area">
+                    <form method="post">
+                        <label for="upload_image">
+                            <button class="btn btn-primary" id="uploaded_image" class="img-responsive img-circle">Загрузить фото</button>
+                            <div class="overlay">
+                                <div class="text">Click to Change Profile Image</div>
+                            </div>
+                            <input type="file" name="image" class="image" id="upload_image" style="display:none"/>
+                        </label>
+                    </form>
+                </div>
+            </div>
+            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel"
+                 aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Кадрирование фото</h5>
+                        </div>
+                        <div class="modal-body">
+                            <div class="img-container">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <img src="" id="sample_image"/>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="preview"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" id="crop" class="btn btn-primary">Применить</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            $(document).ready(function () {
+                var $modal = $('#modal');
+                var image = document.getElementById('sample_image');
+                var cropper;
+
+                $('#upload_image').change(function (event) {
+                    var files = event.target.files;
+
+                    var done = function (url) {
+                        image.src = url;
+                        $modal.modal('show');
+                    };
+
+                    if (files && files.length > 0) {
+                        reader = new FileReader();
+                        reader.onload = function (event) {
+                            done(reader.result);
+                        };
+                        reader.readAsDataURL(files[0]);
+                    }
+                });
+
+                $modal.on('shown.bs.modal', function () {
+                    cropper = new Cropper(image, {
+                        aspectRatio: 1,
+                        viewMode: 3,
+                        preview: '.preview'
+                    });
+                }).on('hidden.bs.modal', function () {
+                    cropper.destroy();
+                    cropper = null;
+                });
+
+                $('#crop').click(function () {
+                    canvas = cropper.getCroppedCanvas({
+                        width: 400,
+                        height: 400
+                    });
+
+                    canvas.toBlob(function (blob) {
+                        url = URL.createObjectURL(blob);
+                        var reader = new FileReader();
+                        reader.readAsDataURL(blob);
+                        reader.onloadend = function () {
+                            var base64data = reader.result;
+                            $.ajax({
+                                url: 'crop.php',
+                                method: 'POST',
+                                data: {image: base64data},
+                                success: function (data) {
+                                    $modal.modal('hide');
+                                    $('#uploaded_image').attr('src', data);
+                                }
+                            });
+                        };
+                    });
+                });
+
+            });
+        </script>
 <?php require_once('../source/footer.php'); ?>
